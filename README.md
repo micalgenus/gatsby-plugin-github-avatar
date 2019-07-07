67ee643475462139cb01fd537168db382e8bc8c1
Micalgenus <micalgenus@gmail.com>
2019-07-07 13:52:18 +0900

    Update Readme

--- README.md
+++ README.md
@@ -3,3 +3,43 @@
 [![Build Status](https://travis-ci.org/micalgenus/gatsby-plugin-github-avatar.svg?branch=master)](https://travis-ci.org/micalgenus/gatsby-plugin-github-avatar)
 [![npm version](https://img.shields.io/npm/v/@micalgenus/gatsby-plugin-github-avatar.svg)](https://www.npmjs.com/package/@micalgenus/gatsby-plugin-github-avatar)
 [![npm total downloads](https://img.shields.io/npm/dt/@micalgenus/gatsby-plugin-github-avatar.svg?style=flat)](https://www.npmjs.com/package/@micalgenus/gatsby-plugin-github-avatar)
+
+## Install
+
+```
+yarn add @micalgenus/gatsby-plugin-github-avatar
+or
+npm install --save @micalgenus/gatsby-plugin-github-avatar
+```
+
+## How to use
+
+```js
+// gatsby-config.js
+
+module.exports = {
+  plugins: [
+    {
+      resolve: `@micalgenus/gatsby-plugin-github-avatar`,
+      options: {
+        username: `micalgenus`, // Github username
+      },
+    },
+  ],
+};
+```
+
+### Example usage
+
+```jsx
+// example.jsx
+
+import React from 'react';
+import Avatar from '@micalgenus/gatsby-plugin-github-avatar';
+
+export class ExampleComponent extends React.Component {
+  render() {
+    return <Avatar />;
+  }
+}
+```
--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,6 @@ import axios from 'axios';
 
 import { GithubAvatarProps, GithubAvatarState } from '../typings';
 
-// import PropTypes from 'prop-types';
-
 declare const GATSBY_GITHUB_USERNAME: string | undefined;
 
 export default class GithubAvatar extends Component<GithubAvatarProps, GithubAvatarState> {
@@ -38,24 +36,3 @@ export default class GithubAvatar extends Component<GithubAvatarProps, GithubAva
     return <img className={className} src={imagePath} alt="Github avatar" />;
   }
 }
-
-// Disqus.propTypes = {
-//   /*
-//    * Tells the Disqus service how to identify the current page.
-//    * When the Disqus embed is loaded, the identifier is used to look up
-//    * the correct thread.
-//    */
-//   identifier: PropTypes.string,
-//   /*
-//    * Tells the Disqus service the title of the current page.
-//    * This is used when creating the thread on Disqus.
-//    */
-//   title: PropTypes.string,
-//   /*
-//    * Tells the Disqus service the URL of the current page.
-//    * This URL is used when a thread is created so that Disqus knows which
-//    * page a thread belongs to.
-//    * (If undefined, Disqus will use the global.location.href)
-//    */
-//   url: PropTypes.string,
-// };
